2ccea34aa5c0f05afbc3f622096dae2ccfc2e9e2
yorni <yorni@inbox.ru>
2022-05-13 11:57:07 +0300

    single object method added for depth

--- routes/depth.js
+++ routes/depth.js
@@ -6,6 +6,11 @@ const depth = require("../models/depth");
 router.get("/:ticker/:starttime", getDepth, (req, res) => {
   res.json(res.depth);
 });
+//Get One
+router.get("/so/:ticker/:starttime", getDepthSingleObject, (req, res) => {
+  res.json(res.depth);
+});
+
 async function getDepth(req, res, next) {
   let depthObject;
   // console.log(req.params);
@@ -49,6 +54,66 @@ async function getDepth(req, res, next) {
     Object.keys(depthObject[0].asks).forEach((ask) => {
       if (Number(ask) <= maxAsk) {
         depthRes.asks.push([ask, depthObject[0].asks[ask]]);
+      } else {
+        throw BreakException;
+      }
+    });
+  } catch (e) {
+    if (e !== BreakException) throw e;
+  }
+
+  res.depth = depthRes;
+  next();
+}
+
+async function getDepthSingleObject(req, res, next) {
+  let depthObject;
+  // console.log(req.params);
+  try {
+    depthObject = await depth
+      .find({
+        ticker: req.params.ticker,
+        time: { $lte: Number(req.params.starttime) },
+      })
+      .sort({ time: -1 })
+      .limit(1);
+    if (!depthObject[0]) {
+      return res.status(404).json({ message: "Cannot find depthObject" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+  ////////////////
+  depthRes = [];
+
+  ////////////////
+  let minBid = Number(Object.keys(depthObject[0].bids)[0]) / 1.05;
+  let maxAsk = Number(Object.keys(depthObject[0].asks)[0]) * 1.05;
+  var BreakException = {};
+  try {
+    Object.keys(depthObject[0].bids).forEach((bid) => {
+      if (Number(bid) >= minBid) {
+        lBid = depthObject[0].bids[bid];
+        lAsk = 0;
+        if (depthObject[0].asks[bid]) {
+          lAsk = depthObject[0].asks[bid];
+          delete depthObject[0].asks[bid];
+        }
+        depthRes.push([bid, lBid, lAsk]);
+      } else {
+        throw BreakException;
+      }
+    });
+  } catch (e) {
+    if (e !== BreakException) throw e;
+  }
+
+  try {
+    Object.keys(depthObject[0].asks).forEach((ask) => {
+      if (Number(ask) <= maxAsk) {
+        lBid = 0;
+        lAsk = depthObject[0].asks[ask];
+        depthRes.push([ask, lBid, lAsk]);
       } else {
         throw BreakException;
       }
