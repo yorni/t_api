c5335041fe82b379a1b55d93b1a0b54f20c3202b
yorni <yorni@inbox.ru>
2022-05-14 09:22:24 +0300

    depth to percent endpoint added

--- routes/depth.js
+++ routes/depth.js
@@ -11,6 +11,15 @@ router.get("/so/:ticker/:starttime", getDepthSingleObject, (req, res) => {
   res.json(res.depth);
 });
 
+//Get One to percent
+router.get(
+  "/sop/:ticker/:starttime/:percent",
+  getDepthSingleObjectToPercent,
+  (req, res) => {
+    res.json(res.depth);
+  }
+);
+
 async function getDepth(req, res, next) {
   let depthObject;
   // console.log(req.params);
@@ -63,6 +72,72 @@ async function getDepth(req, res, next) {
   }
 
   res.depth = depthRes;
+  next();
+}
+
+async function getDepthSingleObjectToPercent(req, res, next) {
+  let depthObject;
+  // console.log(req.params);
+  try {
+    depthObject = await depth
+      .find({
+        ticker: req.params.ticker,
+        time: { $lte: Number(req.params.starttime) },
+      })
+      .sort({ time: -1 })
+      .limit(1);
+    if (!depthObject[0]) {
+      return res.status(404).json({ message: "Cannot find depthObject" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+  ////////////////
+  depthRes = [];
+
+  ////////////////
+  let percent = req.params.percent;
+  let minBid =
+    Number(Object.keys(depthObject[0].bids)[0]) / (1 + percent / 100);
+  let maxAsk =
+    Number(Object.keys(depthObject[0].asks)[0]) * (1 + percent / 100);
+  var BreakException = {};
+  try {
+    Object.keys(depthObject[0].bids).forEach((bid) => {
+      if (Number(bid) >= minBid) {
+        lBid = depthObject[0].bids[bid];
+        lAsk = 0;
+        if (depthObject[0].asks[bid]) {
+          lAsk = depthObject[0].asks[bid];
+          delete depthObject[0].asks[bid];
+        }
+        depthRes.push([bid, lBid, lAsk]);
+      } else {
+        throw BreakException;
+      }
+    });
+  } catch (e) {
+    if (e !== BreakException) throw e;
+  }
+
+  try {
+    Object.keys(depthObject[0].asks).forEach((ask) => {
+      if (Number(ask) <= maxAsk) {
+        lBid = 0;
+        lAsk = depthObject[0].asks[ask];
+        depthRes.push([ask, lBid, lAsk]);
+      } else {
+        throw BreakException;
+      }
+    });
+  } catch (e) {
+    if (e !== BreakException) throw e;
+  }
+
+  res.depth = depthRes.sort(function (a, b) {
+    return Number(a[0]) - Number(b[0]);
+  });
+
   next();
 }
 
