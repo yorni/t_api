1dcd1c4c45c0045bb266d7d4499171cc55551fd4
yorni <yorni@inbox.ru>
2022-05-17 11:23:24 +0300

    added route for cumulative data

--- routes/lastDepthWithActivity.js
+++ routes/lastDepthWithActivity.js
@@ -31,7 +31,7 @@ async function getDepthWithActivity(req, res, next) {
   conditionsToFind = {
     ticker: req.params.ticker,
     time: {
-      $gte: tradesTime - 10000,
+      $gte: tradesTime - 100000,
       $lte: tradesTime,
     },
   };
