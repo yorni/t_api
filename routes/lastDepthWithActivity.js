d5e16012be04deadf9b2f88194b7ac573073adf8
yorni <yorni@inbox.ru>
2022-05-18 09:35:40 +0300

    only trades for cumulative data added

--- routes/lastDepthWithActivity.js
+++ routes/lastDepthWithActivity.js
@@ -7,6 +7,10 @@ const trade = require("../models/trade");
 router.get("/:ticker/:percent/:endtime", getDepthWithActivity, (req, res) => {
   res.json(res.result);
 });
+//Get One to percent
+router.get("activity/:ticker/:endtime", getActivity, (req, res) => {
+  res.json(res.result);
+});
 
 async function getDepthWithActivity(req, res, next) {
   let depthObject;
@@ -168,70 +172,90 @@ async function getDepthWithActivity(req, res, next) {
   next();
 }
 
-async function getDepthSingleObject(req, res, next) {
-  let depthObject;
-
+async function getActivity(req, res, next) {
+  let tradesTime = Number(req.params.endtime);
   let conditionsToFind = {
     ticker: req.params.ticker,
+    time: {
+      $gte: tradesTime - 60000,
+      $lte: tradesTime,
+    },
   };
 
-  if (req.params.starttime != "0") {
-    conditionsToFind.time = {
-      $lte: Number(req.params.starttime),
-    };
-  }
-
+  let tradesRes = {};
   try {
-    depthObject = await depth
-      .find(conditionsToFind)
-      .sort({ time: -1 })
-      .limit(1);
-    if (!depthObject[0]) {
-      return res.status(404).json({ message: "Cannot find depthObject" });
+    tradesPerPeriod = await trade.find(conditionsToFind);
+    if (!tradesPerPeriod[0]) {
+      return res.status(404).json({ message: "Cannot find tradesPerPeriod" });
     }
   } catch (err) {
     return res.status(500).json({ message: err.message });
   }
-  ////////////////
-  depthRes = [];
 
-  ////////////////
-  let minBid = Number(Object.keys(depthObject[0].bids)[0]) / 1.05;
-  let maxAsk = Number(Object.keys(depthObject[0].asks)[0]) * 1.05;
-  var BreakException = {};
-  try {
-    Object.keys(depthObject[0].bids).forEach((bid) => {
-      if (Number(bid) >= minBid) {
-        lBid = depthObject[0].bids[bid];
-        lAsk = 0;
-        if (depthObject[0].asks[bid]) {
-          lAsk = depthObject[0].asks[bid];
-          delete depthObject[0].asks[bid];
-        }
-        depthRes.push([bid, lBid, lAsk]);
+  tradesPerPeriodCumulative = {};
+  tradesPerPeriod.forEach((trade) => {
+    keyTime = String(trade.creationTime);
+    if (!(keyTime in tradesPerPeriodCumulative)) {
+      tradesPerPeriodCumulative[keyTime] = {
+        volumeBuy: 0,
+        volumeSell: 0,
+      };
+
+      if (trade.buyerMaker) {
+        tradesPerPeriodCumulative[keyTime].volumeSell =
+          tradesPerPeriodCumulative[keyTime].volumeSell + trade.volume;
       } else {
-        throw BreakException;
+        tradesPerPeriodCumulative[keyTime].volumeBuy =
+          tradesPerPeriodCumulative[keyTime].volumeBuy + trade.volume;
       }
-    });
-  } catch (e) {
-    if (e !== BreakException) throw e;
-  }
+    }
+  });
 
-  try {
-    Object.keys(depthObject[0].asks).forEach((ask) => {
-      if (Number(ask) <= maxAsk) {
-        lBid = 0;
-        lAsk = depthObject[0].asks[ask];
-        depthRes.push([ask, lBid, lAsk]);
-      } else {
-        throw BreakException;
-      }
-    });
-  } catch (e) {
-    if (e !== BreakException) throw e;
-  }
+  Object.keys(tradesPerPeriodCumulative).forEach((creationTimeKey) => {
+    keyTime = creationTimeKey.slice(0, -3);
+    qtyMarketBuy = 0;
+    qtyMarketSell = 0;
+    volMarketBuy = tradesPerPeriodCumulative[creationTimeKey].volumeBuy;
+    volMarketSell = tradesPerPeriodCumulative[creationTimeKey].volumeSell;
 
-  res.depth = depthRes.sort(function (a, b) {
+    if (volMarketBuy != 0) {
+      qtyMarketBuy = 1;
+    }
+    if (volMarketSell != 0) {
+      qtyMarketSell = 1;
+    }
+
+    if (!(keyTime in tradesRes)) {
+      tradesRes[keyTime] = {
+        qtyMarketBuy: 0,
+        qtyMarketSell: 0,
+        volMarketBuy: 0,
+        volMarketSell: 0,
+      };
+    }
+
+    tradesRes[keyTime].qtyMarketBuy =
+      tradesRes[keyTime].qtyMarketBuy + qtyMarketBuy;
+    tradesRes[keyTime].qtyMarketSell =
+      tradesRes[keyTime].qtyMarketSell + qtyMarketSell;
+    tradesRes[keyTime].volMarketBuy =
+      tradesRes[keyTime].volMarketBuy + volMarketBuy;
+    tradesRes[keyTime].volMarketSell =
+      tradesRes[keyTime].volMarketSell + volMarketSell;
+  });
+
+  tradesResArr = [];
+  Object.keys(tradesRes).forEach((time) => {
+    tradesResArr.push([
+      time,
+      tradesRes[time].qtyMarketBuy,
+      tradesRes[time].qtyMarketSell,
+      tradesRes[time].volMarketBuy,
+      tradesRes[time].volMarketSell,
+    ]);
+  });
+
+  res.result = tradesResArr.sort(function (a, b) {
     return Number(a[0]) - Number(b[0]);
   });
 
