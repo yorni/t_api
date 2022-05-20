a1f398c905fde06f4e58e794df7c8db9acfd2758
yorni <yorni@inbox.ru>
2022-05-20 13:50:21 +0300

    trades added to la

--- routes/lastDepthWithActivity.js
+++ routes/lastDepthWithActivity.js
@@ -190,6 +190,35 @@ async function getDepthWithActivity(req, res, next) {
     ]);
   });
 
+  let tradesObject;
+  try {
+    tradesObject = await trade.find({
+      ticker: req.params.ticker,
+      time: {
+        $gte: tradesTime - 10000,
+        $lte: tradesTime,
+      },
+    });
+
+    if (tradesObject == null) {
+      return res.status(404).json({ message: "Cannot find tradesObject" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+  let tradesGraphArray = tradesObject.map(function (trade) {
+    return [
+      trade.time,
+      trade.price,
+      trade.volume,
+      1, // All deals quantity
+      trade.buyerMaker ? 1 : 0, // BuyerMaker deals quantity
+      trade.buyerMaker ? 0 : 1, // ! BuyerMaker deals quantity
+      trade.buyerMaker ? trade.volume : 0, // BuyerMaker deals vol
+      trade.buyerMaker ? 0 : trade.volume, // ! BuyerMaker deals vol
+    ];
+  });
+
   res.result = {
     depth: depthRes.sort(function (a, b) {
       return Number(a[0]) - Number(b[0]);
@@ -197,6 +226,7 @@ async function getDepthWithActivity(req, res, next) {
     trades: tradesResArr.sort(function (a, b) {
       return Number(a[0]) - Number(b[0]);
     }),
+    tradesGraph: tradesGraphArray,
   };
 
   next();
