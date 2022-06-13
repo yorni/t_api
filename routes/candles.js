8f9bee9920dd584a9ab93a2202e3bae490b36c20
yorni <yorni@inbox.ru>
2022-06-13 17:18:22 +0300

    deals loading added

--- routes/candles.js
+++ routes/candles.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const candle = require("../models/candle");
-
+const dealM = require("./models/deal");
 //Get One
 router.get("/:ticker/:starttime/:endtime", getTrades, (req, res) => {
-  res.json(res.trades);
+  res.json({ trades: res.trades, deals: res.deals });
 });
 async function getTrades(req, res, next) {
   let candlesObject;
@@ -38,7 +38,40 @@ async function getTrades(req, res, next) {
     ];
   });
 
+  let dealsObject;
+  try {
+    dealsObject = await dealM.find({
+      ticker: req.params.ticker,
+      timeOpen: {
+        $gte: Number(req.params.starttime),
+        $lte: Number(req.params.endtime),
+      },
+    });
+
+    if (dealsObject == null) {
+      return res.status(404).json({ message: "Cannot find dealsObject" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+
+  if (dealsObject == null) {
+    dealsArray = [];
+  } else {
+    dealsArray = dealsObject.map(function (deal) {
+      return [
+        deal.timeOpen,
+        deal.timeClose,
+        deal.direction,
+        deal.priceOpen,
+        deal.priceClose,
+        deal.profit,
+      ];
+    });
+  }
+
   res.trades = tradesArray.filter((candle) => candle[1] > 0);
+  res.deals = dealsArray;
   next();
 }
 
