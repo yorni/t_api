dd9cb0675a206bcc6ae0f9895cd2e482b087297a
yorni <yorni@inbox.ru>
2022-06-14 12:52:14 +0300

    log data added

--- listDeals.js
+++ listDeals.js
@@ -10,23 +10,17 @@ db.once("open", () => console.log("connection to db established"));
 
 const dealM = require("./models/deal");
 
-try {
-  dealsObject = await dealM
-    .find({
-      ticker: req.params.ticker,
-      timeOpen: {
-        $gte: Number(req.params.starttime),
-        $lte: Number(req.params.endtime),
-      },
-    })
-    .sort({ timeOpen: 1 });
-
-  if (dealsObject == null) {
-    return res.status(404).json({ message: "Cannot find dealsObject" });
-  }
-} catch (err) {
-  return res.status(500).json({ message: err.message });
-}
-dealsObject.forEach((deal) => {
-  console.log(deal.timeOpen, deal.profit);
-});
+dealM
+  .find({
+    ticker: req.params.ticker,
+    timeOpen: {
+      $gte: Number(req.params.starttime),
+      $lte: Number(req.params.endtime),
+    },
+  })
+  .sort({ timeOpen: 1 })
+  .than((dealsObject) => {
+    dealsObject.forEach((deal) => {
+      console.log(deal.timeOpen, deal.profit);
+    });
+  });
